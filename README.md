80a15197b25fb66761f705b9d0615ba8a63db1b4
Jason Savoie <flimits@gmail.com>
2023-10-20 22:37:51 -0700

    working build. adding readme

--- README.md
+++ README.md
@@ -1,3 +1,258 @@
+# Welcome to Create-Readme
+Where you can automatically genenerate a high-quality README file quickly and relatively easy.
+
+## Description and Motivation
+
+The purpose and motivation behind this appication is to give the user the ability to dynamicaly create a README file. It takes time and effort to create one from scratch, and this app "create-readme" will give them a verty good start.
+
+Create-README is designed to be run on the command line and prompt the user for questions that will help build the README page. 
+Questions such as; Repo Name, Description of app, Outside contributors, licensing/badges and contact information. The app will then assemble all of this information into a usable README that is good enough to slap onto any Git Application.
+
+## Usage
+
+The way you use this Application is:
+- start up a commandline prompt in the directory of the cloned application.
+- Run NPM install to install any dependancies: `npm install`
+- Then execute the app with: `node index.js`
+
+
+## Table of Contents
+
+* [Technology Used](#technology-used)
+* [Description](#description)
+* [Screenshots of Application](screenshots-of-application)
+* [Learning Points](#learning-points)
+* [Code Snippets](#code-snippets)
+* [Contact Info](#contact-info)
+
+## Technology Used 
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| Git | [https://git-scm.com/](https://git-scm.com/)     |    
+| Javascript | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+| Google Search | [https://www.google.com/](https://www.google.com/)
+| Stack Overflow | [https://stackoverflow.com/](https://stackoverflow.com/)
+| Digital Ocean for inquirer Help| [https://www.digitalocean.com/](https://www.digitalocean.com/)
+| W3Schools for JS | [https://www.w3schools.com/](https://www.w3schools.com/)
+| Dev for badges | [https://dev.to/cicirello/badges-tldr-for-your-repositorys-readme-3oo3](https://dev.to/cicirello/badges-tldr-for-your-repositorys-readme-3oo3)
+
+## Description 
+
+[Visit the Deployed Site](https://github.com/flimits/create-readme)
+![img](./assets/image/Banner.png)
+
+Go Nutz Donutz is your one stop shop for everything donut related!
+
+Go Nutz Donutz includes many different ways to interact with the donut community, search for the most convenient location to buy great donutz near you, buy (and sell soon enough) donut merchandise, and discover any fun donut related news you didn't know that you needed.
+
+Our site has:
+
+* Links to articles and games
+* A Donut Locator map that shows you the most popular donut shops near you
+* A Recipe Search so that you can discover popular confectionary recipes
+* Links to the most popular donut merchandise throughout the World Wide Web
+* Blogs for both consumers and sellers to post about custom donut requests!
+
+## Screenshots of Application
+
+Homepage:
+![img](./assets/readme%20images/homepage.png)
+
+Donut Locator:
+![img](./assets/readme%20images/map.png)
+
+Recipes and Blogs:
+![img](./assets/readme%20images/recipes.png)
+
+Merchandise:
+![img](./assets/readme%20images/merch.png)
+
+Donut Blog:
+![img](./assets/readme%20images/blog.png)
+
+## Learning Points 
+
+
+A very important learning point for the whole team was implementing the use of a new CSS Framework, Materialize proved very easy and functional for the team. It had pre-constructed UI elements we ended up utilizing for basically the entirety of the project.
+
+Another thing learned by the team was the ability to use individual html files for seperate pages on a website. Not only did this allow for an easier spread of work delegation but also security behind branching whilst working on only one page at a time.
+
+## Code Snippets
+---
+
+
+Home Page: News Letter.
+This allows us to showcase an article related to donut community. 
+
+![img](./assets/readme%20images/donus-newsletter-image.png)
+
+```html
+</div>
+      <div class="row">
+        <div class="col s12 m7">
+          <div id="NL-card" class="card">
+            <div class="card-image">
+              <img id="NL-pic" src="./assets/image/NLpic.jpg">
+            </div>
+            <div class="card-content">
+              <h2>DUNKIN’® JOY IN CHILDHOOD FOUNDATION® DISTRIBUTES $4.3 MILLION IN GRANTS TO 224 ORGANIZATIONS ACROSS THE COUNTRY</h2>
+              <p id="italisize">Funding from grants will support programs that provide the simple joys of childhood to kids battling hunger or illness</p>
+              <br>
+...
+...
+              <p id="bold">These grants solidify the Dunkin’ Joy in Childhood Foundation’s commitment to bringing the simple joys of childhood to kids battling hunger or illness.</p>
+            </div>
+            <div class="card-action">
+              <a id='bold' class="white-text" href="https://news.dunkindonuts.com/news/dunkin-jicf-2023-grants">Please Check Out The Original Newsletter!</a>
+...
+```
+---
+---
+Search Page: With Geolocation.
+
+```js 
+$(".btn").on("click",function(event){
+    var location = $("#textarea1").val();
+    event.preventDefault();
+    $("iframe.map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyA2Qq9tiWUtSdlkiBJov0EMgRDPTEMKJHw&zoom=7&q=donut+shops+in+" + location);
+})
+```
+![img](./assets/readme%20images/search-page-with-nav.png)
+
+```html 
+        <div id="search-bar" class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label id="placeholder" for="textarea1">Explore The World of Donutz</label>
+                </div>
+              </div>
+              <button id="search-btn" class="btn waves-effect waves-light" type="submit" name="action">Search</button>
+            </form>
+          </div>     
+```
+
+Second here is how we are using Google API to search by that search parameter
+```js
+    <iframe 
+    class="map"
+    frameborder="0" 
+    style="border:0"
+    src="https://www.google.com/maps/embed/v1/search?key=AIzaSyA2Qq9tiWUtSdlkiBJov0EMgRDPTEMKJHw&zoom=7&q=donut+shops+in+United+States">
+    </iframe>
+```
+---
+---
+
+Some things we did for the Donut parafanalia and merchandizing using an Amazon affiliate link.
+
+![img](./assets/readme%20images/Donut-merch.png)
+
+```html
+            <div class="col s12 m4" style="margin-top: 150px;">
+                <div class="merchcard">
+                    <div class="card-image">
+                    <img src="./assets/product images/merch_hat.jpg">
+                    </div>
+                    <div class="card-content">
+                    </div>
+                    <div class="card-action">
+                    <a id="bold" class="white-text" href="https://amzn.to/3FeuqbW">Bucket Hat</a>
+                    </div>
+                </div>
+            </div>
+```
+
+---
+---
+
+The Donut Blog where we have merchants blog about what specials they have and Customers can request specialtiy items or needs they have and merchants can reply or take action on them.
+
+
+![img](./assets/readme%20images/blog.png)
+
+
+
+```html
+                    <div class="card-content">
+                        <div class="row">
+                            <h4 class="state-facts">Make your special requests known!!!</h4>
+                            <div class="input-field col s12">
+                                <textarea id="textarea2" class="blog-text"></textarea>
+                                <label for="textarea2">What do you need?...</label>
+                            </div>
+                            <div id="saveItC"></div>
+                        </div>
+                    </div>
+```
+And associated js...
+
+```js
+// These two are variables to repopulate and get the blog with saved values.
+var blogMCount = getItems("blogM");
+var blogCCount = getItems("blogC");
+
+// We want to check to see if anytihng is being entered by the Merchant here and
+// post it and save it
+$('#textarea1').keypress(function (e) {
+    if (e.which == 13) {
+        var prefx = "Merch";
+        var newDataMerc = $("#textarea1").val();
+        $('#saveItM').append("<p>" + prefx + blogMCount + ": " + newDataMerc + "</p>");
+        postToLocalStore("blogM", newDataMerc, blogMCount++, prefx);
+        $("#textarea1").val("");  // Clear the input field after appending
+        return false;
+    }
+})
+```
+
+---
+---
+
+
+Lastly in the code snippets, is  the Chart.JS for the Donut Poll.
+![img](./assets/readme%20images/donut-poll.png)
+
+```js
+
+new Chart(donutPoll, pollResults);
+
+$(".btn-large").on("click",function(){
+    window.location.reload();
+    if($(this).attr("id") === "poll-btn-donut"){
+        if(!donutData){
+        localStorage.setItem("donutData", 12);
+        localStorage.setItem("doughnutData", 2);
+        }
+        else{
+            localStorage.setItem("donutData", parseInt(donutData)+1);
+        }
+    }
+    else{
+        if(!doughnutData){
+        localStorage.setItem("doughnutData", 2);
+        localStorage.setItem("donutData", 12);
+        }
+        else{
+            localStorage.setItem("doughnutData", parseInt(doughnutData)+1)
+        }
+    }
+});
+```
+
+
+## Contact Info
+
+| Name      |Email      | Github    | Portfolio |
+|-----------|-----------|-----------|-----------|
+|Anna       |           |           |           |
+|Christina  |           |           |           |
+|Jack       |jack.lcmore@gmail.com|https://github.com/JackLCmore|https://jacklcmore.github.io/portfolio/|
+|Jason       |flimits@gmail.com|https://github.com/flimits|https://github.com/flimits/my-portfolio/|
+
+
 # create-readme
 This is a node.js App that will help you create a professional README.md file.
 
--- index.js
+++ index.js
@@ -3,81 +3,74 @@ const fs = require("fs");
 const inquirer = require('inquirer');
 // const { renderLicenseLink, generateMarkdown } = require('./utils/generateMarkdown.js')
 
+goAskTheQuestions = function () {
+    // Array of questions for user input. Used to build README.md
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+    const badgeType = ["GPLv3", "Mozilla", "ODbL", "Perl", "MIT"];
 
-const badgeType = ["GPLv3", "Mozilla", "ODbL", "Perl", "MIT"]
-inquirer
-    .prompt([
-        {
-            message: "Enter your name",
-            name: "yourName",
-            type: "input",
-        },
-        //     message: "Enter your Github Username",
-        //     name: "gitUser",
-        //     type: "input",
-        // },
-        //     message: "What did you learn about the project?",
-        //     name: "learn",
-        //     type: "input",
-        // },
-        // {
-        //     message: "What is the Title of your README?",
-        //     name: "title",
-        //     type: "input",
-        // },
-        // {
-        //     message: "Give description of your README?",
-        //     name: "description",
-        //     type: "input",
-        // },
-        // {
-        //     message: "What is your motivation for this project?",
-        //     name: "motivation",
-        //     type: "input",
-        // },
-        // {
-        //     message: 'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")',
-        //     name: "theWhy",
-        //     type: "input",
-        // },
-        // {
-        //     message: "What does this application solve?",
-        //     name: "solve",
-        //     type: "input",
-        // },
-        // {
-        //     message: "What did you learn about the project?",
-        //     name: "learn",
-        //     type: "input",
-        // },
-        // {
-        //     message: "How do you use this application (what is its usage)?",
-        //     name: "testUsage",
-        //     type: "input",
-        // },
-        {
-            name: "badgeName",
-            type: "rawlist",
-            message: "Pick a  Licensed Badge are you using.",
-            choices: badgeType,
-        },
-    ])
-    .then((data) => {
-        // console.log(data);
-        console.log(data.badgeName);
-        const licenseIs = renderLicenseLink(data.badgeName);
-        console.log("Now that you have the license, we should render it")
-        // console.log(data[0])
+    inquirer
+        .prompt([
+            {
+                message: "Enter your name",
+                name: "yourName",
+                type: "input",
+            },
+            {
+                message: "Enter your Github Username",
+                name: "gitUser",
+                type: "input",
+            },
+            {
+                message: "What is the Title of your README/Application (should match repo)?",
+                name: "title",
+                type: "input",
+            },
+            {
+                message: "Give description of your project.",
+                name: "description",
+                type: "input",
+            },
+            {
+                message: "What is your motivation for this project?",
+                name: "motivation",
+                type: "input",
+            },
+            {
+                message: "Where there any outside contributors to meantion (links too)?",
+                name: "contributions",
+                type: "input",
+            },
+            {
+                message: "How do you install this application? (Step by Step instructions.)",
+                name: "installation",
+                type: "input",
+            },
+            {
+                message: "How do you use this application (what is its usage)?",
+                name: "usage",
+                type: "input",
+            },
+            {
+                name: "badgeName",
+                type: "rawlist",
+                message: "Pick a  Licensed Badge are you using.",
+                choices: badgeType,
+            },
+        ])
+        .then((data) => {
+            // console.log(data);
+            console.log(data.badgeName);
+            const licenseIs = renderLicenseLink(data.badgeName);
+            console.log("Now that you have the license, we should render it")
+            // console.log(data[0])
 
-        // console.log("badge lin " + license.renderLicenseLink(badgeType[0]))
-        const goGetReadme = myReadmeMarkdown(data,licenseIs)
-        console.log("Now go and write it to file")
-        writeToFile("README-NEW.md",goGetReadme);
-    });
-
+            // console.log("badge lin " + license.renderLicenseLink(badgeType[0]))
+            const goGetReadme = myReadmeMarkdown(data, licenseIs)
+            console.log("Now go and write it to file")
+            writeToFile("README-NEW.md", goGetReadme);
+        });
+}
+// This function is to take in the choice for the badge, and use switch case to match up the license.
 function renderLicenseLink(badge) {
     console.log(badge)
 
@@ -107,107 +100,92 @@ function renderLicenseLink(badge) {
     }
 
     console.log("The license chosen is " + badge);
-    // writeToFile(license);
-    // console.log("readmefile " + myReadmeMarkdown)
-    // writeToFile("readme-new.md",myReadmeMarkdown);
 
     return badge;
 }
 
-// console.log("show license information " + renderLicenseLink);
 
-// // TODO: Create a function to write README file
+// This function is to write README file to local disk
 function writeToFile(fileName, markdownData) {
 
     fs.writeFile(fileName, markdownData, (err) =>
-        err ? console.error(err) : console.log('Success!')
+        err ? console.error(err) : console.log('Successfully wrote ' + fileName + ' readme file!')
     )
 };
-// // TODO: Create a function to initialize app
-// function init() {}
 
-// // Function call to initialize app
-// init();
-
-const myReadmeMarkdown = function (data,licenseIs) {
+const myReadmeMarkdown = function (data, licenseIs) {
     const title = data.title || "TITLE";
+    const gitUser = data.gitUser || "GITHUB USER";
+    const email = data.email || "PUT YOUR EMAIL HERE";
     const description = data.description || "DESCRIPTION";
     const motivation = data.motivation || "MOTIVATION";
-    const theWhy = data.theWhy || "THE WHY";
-    const solve = data.solve || "WHAT IS IT GOING TO SOLVE?";
-    const learn = data.learn || "WHAT DID YOU LEARN?";
-    const testUsage = data.testUsage || "ENTER THE TEST USAGE CASE";
+    const installation = data.learn || "ADD INSTALLATION HERE";
+    const usage = data.usage || "USAGE: HOW YOU RUN THIS CODE";
     const license = licenseIs || "HERE IS WHERE THE BADGE GOES";
+    const contributions = data.contributions || "HERE IS WHERE THE CONTRIBUTORS TO YOUR PAGE GO";
 
-    const readMeTemplate = `# ${title}
+    const readMeTemplate = `# Welcome to ${title}
                         
-                        ## Description
-                        
-                        ${description}:
-                        
-                        - ${motivation}
-                        - ${theWhy}
-                        - What problem does it solve?
-                        - What did you learn?
-                        
-                        ## Table of Contents (Optional)
-                        
-                        If your README is long, add a table of contents to make it easy for users to find what they need.
-                        
-                        - [Installation](#installation)
-                        - [Usage](#usage)
-                        - [Credits](#credits)
-                        - [${license}
-                        
-                        ## Installation
-                        
-                        What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-                        
-                        ## Usage
-                        
-                        Provide instructions and examples for use. Include screenshots as needed.
-                        
-                        To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-                        
-                        \`\`\`md
-                        ![alt text](assets/images/screenshot.png)
-                        \`\`\`
-                        
-                        ## Credits
-                        
-                        List your collaborators, if any, with links to their GitHub profiles.
-                        
-                        If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-                        
-                        If you followed tutorials, include links to those here as well.
-                        
-                        ## License
-                        
-                        The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-                        
-                        ---
-                        
-                        🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-                        
-                        ## Badges
-                        
-                        ${license}
-                        ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-                        
-                        Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-                        
-                        ## Features
-                        
-                        If your project has a lot of features, list them here.
-                        
-                        ## How to Contribute
-                        
-                        If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-                        
-                        ## Tests
-                        
-                        Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-                        `
-                        return readMeTemplate;
+## Description
+
+- ${description}:
+- ${motivation}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [GitHub Project](#github-project)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## GitHub project.
+
+This project will be deployed at: [https://${gitUser}.github.io/${title}](https://${gitUser}.github.io/${title})
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Credits
+
+${contributions}
+
+## License
+
+Here is a list of licensing and badges associaged with them that you use in this application.
+
+## Badges
+
+${license}
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+
+## Tests
+
+
+## Contact Me
+
+You may contact me can follow my Github Projects  through:
+<a href="https://github.com/${gitUser}/">https://github.com/${gitUser}/</a>
+       OR
+Email me at: <${email}>
+
+### Thank you for stoppying by!
+`
+    return readMeTemplate;
 };
-// writeToFile("NEW-README.md");
+
+// Function to initialize app
+function init() {
+    goAskTheQuestions();
+}
+
+// Function call to initialize app
+init();
